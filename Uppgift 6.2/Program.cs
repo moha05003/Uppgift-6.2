b0fe9405410bd516c912c4991a6f6ee388a3609c
Elev <Elev@TEB125104>
2023-03-03 14:37:10 +0100

    Add project files.

---
+++ Uppgift 6.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace inllämningsuppgift
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(störst(6, 7));
+
+            Console.WriteLine(störst(2, 6));
+
+            Console.WriteLine(störst(65, 65));
+        }
+        static int störst(int tal1, int tal2)
+        {
+            if (tal1 == tal2)
+            {
+                return tal1;
+            }
+            else if (tal1 > tal2)
+            {
+                return tal1;
+
+            }
+            else
+                return tal2;
+        }
+    }
+}
